c625b28fe5c6d03d0c2ba5d6deb73c20c4535ff8
maizaaaa <maizatulhasanah02@gmail.com>
2026-04-25 22:08:04 +0800

    Update exercise for deadlock

---
+++ src/main/java/Week_06/Deadlock/Account.java
@@ -0,0 +1,25 @@
+package Week_06.Deadlock;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Account {
+    private final AtomicInteger balace;
+    private final String name;
+
+    public Account(String name, int balace) {
+        this.name = name;
+        this.balace = new AtomicInteger(balace);
+    }
+
+    public void withdraw(int amount){
+        balace.addAndGet(-amount);
+    }
+
+    public void deposit(int amount){
+        balace.addAndGet(amount);
+    }
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/java/Week_06/Deadlock/Deadlock.java
@@ -0,0 +1,33 @@
+package Week_06.Deadlock;
+
+public class Deadlock {
+    public static void transfer (Account from, Account to, int amount){
+        Account first = from.getName().compareTo(to.getName()) < 0?from:to;
+        Account second = first == from ? to : from;
+
+        synchronized (first){
+            System.out.println(Thread.currentThread().getName()+" locked "+from.getName());
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {}
+
+                synchronized (second){
+                    System.out.println(Thread.currentThread().getName()+" locked "+to.getName());
+
+                    from.withdraw(amount);
+                    to.deposit(amount);
+                }
+        }
+    }
+
+    public static void main(String[] args) {
+        Account A = new Account("A", 1000);
+        Account B = new Account("B", 1000);
+
+        Thread t1 = new Thread(() -> transfer (A, B, 100));
+        Thread t2 = new Thread(() -> transfer (B, A, 200));
+
+        t1.start();
+        t2.start();
+    }
+}
